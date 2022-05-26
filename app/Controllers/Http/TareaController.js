62b9714590f25fa6c431fddf704d9f0c17c08a78
AndresTam <19030296@itcelaya.edu.mx>
2022-05-25 19:34:32 -0500

    Creación de los métodos Actualizr y Eliminar tareas

--- app/Controllers/Http/TareaController.js
+++ app/Controllers/Http/TareaController.js
@@ -15,15 +15,39 @@ class TareaController {
 
     async create({ auth, request, params }) {
         const user = await auth.getUser()
-        const { description } = request.all()
+        const { descripcion } = request.all()
         const { id } = params
         const proyecto = await Proyecto.find(id)
         AutorizacionService.verificarPermiso(proyecto, user)
         const tarea = new Tarea()
         tarea.fill({
-            description
+            descripcion
         })
         await proyecto.tareas().save(tarea)
+        return tarea
+    }
+
+    async update({ auth, params, request }) {
+        const user = await auth.getUser()
+        const { id } = params
+        const tarea = await Tarea.find(id)
+        const proyecto = await tarea.proyecto().fetch()
+        AutorizacionService.verificarPermiso(proyecto, user)
+        tarea.merge(request.only([
+            'descripcion',
+            'completada'
+        ]))
+        await tarea.save()
+        return tarea
+    }
+
+    async destroy({ auth, params, request }) {
+        const user = await auth.getUser()
+        const { id } = params
+        const tarea = await Tarea.find(id)
+        const proyecto = await tarea.proyecto().fetch()
+        AutorizacionService.verificarPermiso(proyecto, user)
+        await tarea.delete()
         return tarea
     }
 }
